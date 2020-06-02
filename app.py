370b85ef84dd5a6d647a95a6a00ac7ce52aab59a
roym <manish.roy@sms-group.com>
2020-06-02 14:49:44 +0530

    Removed Boot strap

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import flask
 
 
 server = flask.Flask(__name__)
-app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.CYBORG])
+app = dash.Dash(__name__, server=server)
 app.config.suppress_callback_exceptions = True
 server = app.server
 
