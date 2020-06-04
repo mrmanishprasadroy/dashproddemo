370d8a7f6487918bb951eab1854b199030fc4bf3
roym <manish.roy@sms-group.com>
2020-06-04 08:20:54 +0530

    Added basic Authentication

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import dash
-import dash_html_components as html
+import dash_auth
 import dash_bootstrap_components as dbc
 import flask
 
 
+# Keep this out of source code repository - save in a file or a database
+VALID_USERNAME_PASSWORD_PAIRS = {
+    'admin': '!sysadmin'
+}
 server = flask.Flask(__name__)
 app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.DARKLY])
 app.config.suppress_callback_exceptions = True
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
